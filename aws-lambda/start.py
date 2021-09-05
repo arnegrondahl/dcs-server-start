88f2bebb9b4f3e81ef9e815de7af4393007d00e7
agrondahl <83029764+agrondahl@users.noreply.github.com>
2021-09-05 16:22:28 +0200

    Create start.py

---
+++ aws-lambda/start.py
@@ -0,0 +1,33 @@
+# Starts an EC2 instance and reports back to Discord. I use it for scheduling automagic
+# start/stops using AWS CloudWatch rules.
+# This needs packaging with dependencies before uploading to a Lamdba rule.. see 
+# https://docs.aws.amazon.com/lambda/latest/dg/python-package.html for ezpz steps.
+
+import requests
+import boto3
+
+region = 'eu-north-1'
+instances = ['i-31337'] # Instance ID here!
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, context):
+    ec2.start_instances(InstanceIds=instances)
+
+    url = "https://discord.com/api/webhooks/yourwebeliwebhookhere" # Your webhook here.
+
+    data = {
+        "content" : "Scheduled automation is starting server Viktor Röd",
+        "username" : "Viktor Röd Server"
+    }
+
+    result = requests.post(url, json = data)
+
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print("Payload delivered successfully, code {}.".format(result.status_code))
+    
+    print('stopped your instances: ' + str(instances))
+   
